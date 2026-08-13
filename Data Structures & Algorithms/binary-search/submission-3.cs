1b9b6dee8549dcff08f3723142bdcf9d6a0f58ab
Harshavardhana G M <140304245+harshavardhana-37@users.noreply.github.com>
2026-08-13 07:52:24 +0530

    Add: binary-search - submission-3

---
+++ Data Structures & Algorithms/binary-search/submission-3.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int Search(int[] nums, int target) {
+        int left=0;
+        int n=nums.Length;
+        int right=n-1;
+        int mid;
+        while(left<=right){
+            mid=left+(right-left)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>target){
+                 right=mid-1;
+            }
+            else{
+               left=mid+1;
+            }
+
+        }
+        return -1;
+        
+    }
+}
